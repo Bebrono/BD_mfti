0de5e384c9df96433e1e762e1907906dec5b2cca
Savchik <162825917+Bebrono@users.noreply.github.com>
2025-05-26 23:16:19 +0500

    Delete Script-4.sql

--- Script-4.sql
+++
@@ -1,9 +0,0 @@
---Клиенты, сделавшие более 2-х бронирований
-SELECT 
-    c.full_name,
-    COUNT(b.booking_id) AS total_bookings
-FROM clients c
-JOIN booking b ON c.client_id = b.client_id
-GROUP BY c.client_id
-HAVING COUNT(b.booking_id) >= 1
-ORDER BY total_bookings DESC;
