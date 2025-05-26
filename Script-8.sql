3f3da1e912cdc651c1cede7c54936fca3de89bad
Savchik <162825917+Bebrono@users.noreply.github.com>
2025-05-26 23:15:51 +0500

    Delete Script-8.sql

--- Script-8.sql
+++
@@ -1,8 +0,0 @@
---Рейтинг клиентов по количеству бронирований (с оконной функцией)
-SELECT 
-    c.full_name,
-    COUNT(b.booking_id) AS booking_count,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
-FROM clients c
-JOIN booking b ON c.client_id = b.client_id
-GROUP BY c.client_id;
