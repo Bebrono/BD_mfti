b3880bef84cd84ccd36f07762a44e12cd12dc40e
Savchik <162825917+Bebrono@users.noreply.github.com>
2025-05-26 23:15:58 +0500

    Delete Script-7.sql

--- Script-7.sql
+++
@@ -1,5 +0,0 @@
---Найти клиентов, которые бронировали номер хотя бы один раз в июле 2024
-SELECT DISTINCT c.full_name
-FROM clients c
-JOIN booking b ON c.client_id = b.client_id
-WHERE b.start_date BETWEEN '2024-07-01' AND '2024-07-31';
