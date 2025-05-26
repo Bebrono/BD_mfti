ae29c85262ed25532d22e78645087f74f102e569
Savchik <162825917+Bebrono@users.noreply.github.com>
2025-05-26 23:16:26 +0500

    Delete Script-3.sql

--- Script-3.sql
+++
@@ -1,8 +0,0 @@
---Количество бронирований по каждому тарифу
-SELECT 
-    t.name AS tariff_name,
-    COUNT(*) AS booking_count
-FROM booking b
-JOIN tariffs t ON b.tariff_id = t.tariff_id
-GROUP BY t.name
-ORDER BY booking_count DESC;
